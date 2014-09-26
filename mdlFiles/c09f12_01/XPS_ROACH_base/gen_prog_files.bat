d8594bb897f780ddcd385a7b14666b54ae5732aa
charles Copley <charlescopley@gmail.com>
2014-09-26 09:21:04 +0200

    removed c09 bit files

--- mdlFiles/c09f12_01/XPS_ROACH_base/gen_prog_files.bat
+++
@@ -1,3 +0,0 @@
-copy implementation\system.bit ..\bit_files\c09f12_01_2014_Aug_08_1735.bit
-mkbof.exe -o implementation\system.bof -s core_info.tab -t 3 implementation\system.bin
-copy implementation\system.bof ..\bit_files\c09f12_01_2014_Aug_08_1735.bof
